cc970e0006301db447a030e34639a2b6ab81b4bb
R.S.K.Abbas <rskabbas@outlook.com>
2021-05-22 21:14:47 +0530

    Add files via upload

---
+++ day7/three.js
@@ -0,0 +1,5 @@
+function regexVar() {
+    
+    var re = RegExp('\\d+','g')
+    return re;
+}
---
+++ day7/two.js
@@ -0,0 +1,5 @@
+function regexVar() {
+    
+    var re = (/^(Mr\.|Dr\.|Er\.|Ms\.|Mrs\.)\s?[a-z|A-Z]+$/);
+    return re;
+}
